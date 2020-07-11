df7e9194bad65520e92f809154b4bf8f2e8b8527
Yevhen Samoilenko <eugene.samoilenko@gmail.com>
2020-07-11 17:50:26 +0300

    final add in lab 5_4

--- Lab_5_4.py
+++ Lab_5_4.py
@@ -1,8 +1,6 @@
 foo = [1, 2, 3, 4, 5]
 isOddl = (lambda x: bool(x % 2 == 1))
-foo1 = [i for i in range(1, 6) if isOddl(i)]
 odd_foo = []
-
 
 def isOdd(x):
     return x % 2 == 1
@@ -11,4 +9,5 @@ for f in foo:
     if isOddl(f):
         odd_foo.append(f)
 print (odd_foo)
-print(foo1)
+
+print([i for i in range(1, 6) if (lambda x: bool(x % 2 == 1))(i)])
