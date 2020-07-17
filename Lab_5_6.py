455e23710c63a5fc4c50dec1d03886c3eb171600
Yevhen Samoilenko <eugene.samoilenko@gmail.com>
2020-07-17 10:01:59 +0300

    after practice lesson

--- Lab_5_6.py
+++ Lab_5_6.py
@@ -1,17 +1,20 @@
 FILENAME = r'C:\Users\cobra\PycharmProjects\MA_prog\MA_aducation\config.data'
 lst = ['string1', 'string2', 'string3', 'string4', 'string5']
 
-def writeConfig(file, string):
-    if 'Configuration file! Do not modify!' in file.read():
-        file.write(string)
-    else:
-        file.write(f'Configuration file! Do not modify! \n{string}')
 
 def myDec(FILENAME, string):
     def writeConfigFile(FILENAME, string):
         with open(FILENAME, 'r+') as file:
             writeConfig(file, string)
     return writeConfigFile(FILENAME, string)
+
+@myDec
+def writeConfig(file, string):
+    if 'Configuration file! Do not modify!' in file.read():
+        file.write(string)
+    else:
+        file.write(f'Configuration file! Do not modify! \n{string}')
+
 
 with open(FILENAME, 'w') as file:
     file.truncate()
---
+++ work3.py
@@ -0,0 +1,54 @@
+import time
+from functools import reduce
+from math import factorial
+from operator import mul, truediv
+
+SEC = 1
+def printer(func):
+    call_counter = 0
+    def wrapped(*args, **kwargs):
+        nonlocal call_counter
+        ctime = time.time()
+        result = func(*args, **kwargs)
+        #print(f'Execution time = {time.time() - ctime - SEC}')
+        call_counter += 1
+        print(f'in wrapped, call_counter = {call_counter}')
+        return result
+    return wrapped
+
+
+@printer
+def some():
+    time.sleep(SEC)
+    return 1
+
+@printer
+def next_some():
+    return 2
+
+def make_averager():
+    lst = []
+    def aver(value: int = None) -> object:
+        if value is not None:
+            lst.append(int(value))
+            v = int(sum(lst) / len(lst))
+            print(v)
+            return v
+        else:
+            print('No params in call!')
+            return 0
+    return aver
+
+avg = make_averager()
+
+
+def fact(n):
+    return reduce(lambda x, y: x * y, range(1, n + 1))
+
+
+def fact1(n):
+    return reduce(mul, range(1, n + 1))
+
+print(fact(4))
+print(fact1(4))
+print(factorial(4))
